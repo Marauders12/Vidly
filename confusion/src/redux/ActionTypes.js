9e4e2bbbd1a90a15bf8ba904464c83aa3139241a
Marauders12 <72773439+Marauders12@users.noreply.github.com>
2020-10-13 06:51:32 +0530

    Add files via upload

---
+++ confusion/src/redux/ActionTypes.js
@@ -0,0 +1,6 @@
+import { DISHES } from "../shared/dishes";
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const DISHES_LOADING='DISHES_LOADING';
+export const ADD_DISHES='ADD_DISHES';
+export const DISHES_FAILED= 'DISHES_FAILED';
